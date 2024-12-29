942bb620f18315cd844de61e6981bd67e290960c
Jiangkuanli <jiangkuanli@163.com>
2024-12-29 09:20:20 +0800

    Add files via upload

--- employeeSystem/src/main/java/com/cn/employeesystem/controller/EmployeeController.java
+++ employeeSystem/src/main/java/com/cn/employeesystem/controller/EmployeeController.java
@@ -1,12 +1,10 @@
 package com.cn.employeesystem.controller;
 
 import com.cn.employeesystem.entity.Employee;
+import com.cn.employeesystem.entity.Page;
 import com.cn.employeesystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,11 +27,34 @@ public class EmployeeController {
 //    @GetMapping("/emps")
     @GetMapping
     public List<Employee> findAllEmp() {
+
         return employeeService.findAllEmployee();
     }
     @GetMapping("/findId/{id}")
     public Employee findEmployeeById(@PathVariable String id) {
+
         return employeeService.findEmployeeById(id);
+    }
+    @PostMapping("/addEmployee")
+    public boolean addEmployee(@RequestBody Employee employee) {
+
+        return employeeService.insertEmployee(employee);
+    }
+    @PostMapping("/updateEmployee")
+    public boolean updateEmployee(@RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
+    }
+    @DeleteMapping("/deleteEmployee/{id}")
+    public boolean deleteEmployee(@PathVariable String id) {
+        return employeeService.deleteEmployee(id);
+    }
+    @GetMapping("/Page")
+    public Page<Employee>getEmployeesByPage(@RequestParam int currentPage, int pageSize) {
+        return employeeService.findEmployeesByPage(currentPage,pageSize);
+    }
+    @DeleteMapping("/delByIds")
+    public boolean deleteItemsByIds(@RequestBody List<Integer> ids) {
+        return employeeService.deleteItemsByIds(ids);
     }
 
 }
