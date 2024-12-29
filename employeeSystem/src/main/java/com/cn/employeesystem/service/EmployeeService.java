783860e88996fba36aeca65a7f276bb668a0b415
Jiangkuanli <jiangkuanli@163.com>
2024-12-29 09:24:59 +0800

    Add files via upload

--- employeeSystem/src/main/java/com/cn/employeesystem/service/EmployeeService.java
+++ employeeSystem/src/main/java/com/cn/employeesystem/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.cn.employeesystem.service;
 
 import com.cn.employeesystem.entity.Employee;
+import com.cn.employeesystem.entity.Page;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -8,5 +10,21 @@ public interface EmployeeService {
     public List<Employee> findAllEmployee();
 
 //    Employee findEmployeeById(int id);
-        Employee findEmployeeById(String id);
+    public Employee findEmployeeById(String id);
+
+    public  boolean insertEmployee(Employee employee);
+
+    public boolean updateEmployee(Employee employee);
+
+
+    public boolean deleteEmployee(String id);
+
+//    Page<Employee> findAllEmployee(int currentPage, int pageSize);
+//
+//    Page<Employee> findEmployeeByPage(int currentPage, int pageSize);
+
+    Page<Employee> findEmployeesByPage(int currentPage, int pageSize);
+
+
+    public boolean deleteItemsByIds(@Param("ids") List<Integer> ids);
 }
